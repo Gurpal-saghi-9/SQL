ddce5c9359ef929260495908235e50ed48226527
guru <gurusaghi324@gmail.com>
2025-01-16 14:05:57 +0530

    commit

--- new.sql
+++ new.sql
@@ -22,3 +22,10 @@ CREATE TABLE
     (3, 'Emily', 'Jones', '1990-03-20', '2015-11-20', 'Analyst', 55000.00, 'Finance');
 
     SELECT * FROM Employees;
+
+    -- Output:
+-- EmployeeID | FirstName | LastName | BirthDate  | HireDate   | Position  | Salary  | Department
+-- ---------------------------------------------------------------------------------------------
+-- 1          | John      | Doe      | 1980-01-01 | 2010-06-01 | Manager   | 75000.00| Sales
+-- 2          | Jane      | Smith    | 1985-02-15 | 2012-09-15 | Developer | 65000.00| IT
+-- 3          | Emily     | Jones    | 1990-03-20 | 2015-11-20 | Analyst   | 55000.00| Finance
